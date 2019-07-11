109ec98b1c8553013eda6b22d6b36e7e350b01e8
Rob Schwartz <rob.schwartz@vibes.com>
2019-07-10 21:49:51 -0400

    progress on conversion

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -10,21 +10,11 @@ class PageController < ActionController::Base
   def search_help
     form_version = ["a","b"]
     version = form_version.sample
+    
     @location = params['location']
-    set_title_tag
+
     puts version
     render "search_help_#{version}"
   end
 
-  private
-
-  def set_title_tag
-    @title = if !@location.blank?
-              "Nursing Homes And Assisted Living Facilities Near #{@location}"
-            elsif !@state.blank?
-              "Nursing Homes, Assisted Living Facilities, Intermediate Care #{@state}"
-            else
-              "Nursing Homes, Assisted Living Facilities, Intermediate Care Near You"
-            end
-  end
 end
